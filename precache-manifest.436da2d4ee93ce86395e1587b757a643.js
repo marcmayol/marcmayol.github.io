cce0b31e962bac5c8a6a96eb7e3a919ad31d4cd0
Marc Mayol Orell <marcmayolorell@gmail.com>
2023-01-29 17:35:55 +0100

    Updates

---
+++ precache-manifest.436da2d4ee93ce86395e1587b757a643.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "07d0ebe682c7896a863b",
+    "url": "/static/css/main.b6aeaf29.chunk.css"
+  },
+  {
+    "revision": "07d0ebe682c7896a863b",
+    "url": "/static/js/main.07d0ebe6.chunk.js"
+  },
+  {
+    "revision": "c765bae6be2a5b9a7cdf",
+    "url": "/static/js/1.c765bae6.chunk.js"
+  },
+  {
+    "revision": "975abfaeacdbfeef4de9",
+    "url": "/static/css/2.fef15201.chunk.css"
+  },
+  {
+    "revision": "975abfaeacdbfeef4de9",
+    "url": "/static/js/2.975abfae.chunk.js"
+  },
+  {
+    "revision": "7a41c24f38333f284b92",
+    "url": "/static/js/runtime~main.7a41c24f.js"
+  },
+  {
+    "revision": "d85bf4405b22448ba62452d8e15eb7e1",
+    "url": "/static/media/google-tag-manager-v.d85bf440.png"
+  },
+  {
+    "revision": "816121b4ab04d12da8509c785a97d2cb",
+    "url": "/static/media/Ga4-header-image.816121b4.webp"
+  },
+  {
+    "revision": "239f8261c49a4a7c110e75dd16c6cdf1",
+    "url": "/static/media/logo1_black_nb.239f8261.svg"
+  },
+  {
+    "revision": "be3eed407d2f0417f323d635bdd3b57a",
+    "url": "/static/media/jumbotron.be3eed40.png"
+  },
+  {
+    "revision": "127f647bf86c85cf74a78a11abe1e1d0",
+    "url": "/static/media/yo.127f647b.png"
+  },
+  {
+    "revision": "3d05087882358cd4c1caede0fc3cc5ba",
+    "url": "/index.html"
+  }
+];
