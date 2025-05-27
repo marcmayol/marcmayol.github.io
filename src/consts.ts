3bc7cd46bc278e80c7d0e9849aac8d579e0a6399
Marc Mayol Orell <marcmayolorell@gmail.com>
2025-05-27 18:18:46 +0200

    migración a astro

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Marc Mayol';
+export const SITE_DESCRIPTION = 'Marc Mayol Gen AI Developer & Researcher conocimientos sobre IA Generativa, productos de Google y desarrollo de inteligencia artificial';
+export const  APPLICATION_NAME = 'Marc Mayol blog personal';
