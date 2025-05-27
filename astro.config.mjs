632bbf591f0f96e69e795f72c4880355387935fb
Marc Mayol Orell <marcmayolorell@gmail.com>
2025-05-27 18:29:32 +0200

    add robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://marcmayol.com/',
+	site: 'https://marcmayol.com',
 	// base:'test-astro',
 	output: 'static',
 	integrations: [mdx(), sitemap()],
